f184fde15beb032d125a5dc97cb631d5d2d0061d
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-04-09 12:36:31 -0400

    adding variables and loop

--- UnicodeAllisonC/UnicodeForm.Designer.cs
+++ UnicodeAllisonC/UnicodeForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UnicodeAllisonC
 {
-    partial class Form1
+    partial class frmUnicode
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumber = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("ISOCPEUR", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(88, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumber
+            // 
+            this.lstNumber.Font = new System.Drawing.Font("ISOCPEUR", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumber.FormattingEnabled = true;
+            this.lstNumber.ItemHeight = 19;
+            this.lstNumber.Location = new System.Drawing.Point(65, 78);
+            this.lstNumber.Name = "lstNumber";
+            this.lstNumber.Size = new System.Drawing.Size(135, 175);
+            this.lstNumber.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstNumber);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Allison C";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumber;
     }
 }
 
--- UnicodeAllisonC/UnicodeForm.cs
+++ UnicodeAllisonC/UnicodeForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 9 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day # - Unicode
+ * This program shows the letters A - Z with the corrisponding unicdoe number
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,28 @@ using System.Windows.Forms;
 
 namespace UnicodeAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmUnicode : Form
     {
-        public Form1()
+        public frmUnicode()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //clearing list box
+            lstNumber.Items.Clear();
+
+            //variables
+            string letter;
+            int counter;
+
+            //looping throught letters A(65) to Z(90)
+            for (counter = 65; counter <= 90; counter++)
+            {
+
+            }
+
         }
     }
 }
